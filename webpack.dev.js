25eed95f5c2a3ca80a0991d58bb4eda2e18a1dca
Nick Zatkovich <NZatkovich@gmail.com>
2018-10-27 15:15:34 -0700

    webpack

---
+++ webpack.dev.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack')
+const path = require('path')
+
+
+
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+
+
+
+module.exports = {
+  target: 'node',
+  module: {
+    rules: [{
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader',
+
+      options: {
+        plugins: ['syntax-dynamic-import'],
+
+        presets: [['@babel/preset-env', {
+		  'targets': {
+			"node": "4.0.0"
+		}
+        }]]
+      },
+
+      test: /\.js$/
+    }]
+  },
+
+  entry: "./index.js",
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'development',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
+}
