977419abd3403530d02e457bfb9ad98f07b17c6d
Nick Zatkovich <NZatkovich@gmail.com>
2019-01-23 14:34:11 -0800

    more topic tests

--- tests/__tests__/rosnodejs-topics-test.js
+++ tests/__tests__/rosnodejs-topics-test.js
@@ -8,12 +8,32 @@ let server;
 beforeAll(async ()=>{
    server = new Server();
    await server.start();
-   nh = await rosnodejs.initNode('/test_rosnodejs_client', {rosMasterUri: "http://localhost:22114"});
+   await rosnodejs.initNode('/test_rosnodejs_client', {rosMasterUri: "http://localhost:22114"});
+   nh = rosnodejs.nh
 })
 
-it('topic pub', done =>{
-    done()
+it('topic advertise', done =>{
+    let ds = rosnodejs.nh
+    let publisher = ds.advertise("/test_advertise1","std_msgs/String")
+    publisher.on('registered', (msg)=>{
+        done();
+    })
 })
+it('topic publish', done =>{
+    let ds = rosnodejs.nh
+    let publisher = ds.advertise("/test_pub","std_msgs/String")
+    publisher.on('registered', ()=>{
+        const subscriber = ds.subscribe("/test_pub", "std_msgs/String", (msg)=>{
+            expect(msg).toBeTruthy()
+            expect(msg.data).toEqual("hello")
+            done();
+        })
+        subscriber.on('registered', ()=>{
+            debug("hello!")
+            publisher.publish({data: "hello"}, -1)
+        })
+    })
+}, 10000)
 afterAll(async ()=>{
     await rosnodejs.shutdown();
     await server.stop();
