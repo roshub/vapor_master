45d3aefc8bed7f023dc86908550ce89db6fca6b5
Nick Zatkovich <NZatkovich@gmail.com>
2019-01-23 11:29:48 -0800

    add services test

---
+++ tests/__tests__/rosnodejs-services-test.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('test.rosnodejs-client.services')
+const Server = require('../../src/server')
+const rosnodejs = require('rosnodejs')
+
+let nh;
+let server;
+
+beforeAll(async ()=>{
+   server = new Server();
+   await server.start();
+   nh = await rosnodejs.initNode('/test_rosnodejs_client', {rosMasterUri: "http://localhost:22114"});
+})
+
+it('register service', done =>{
+    expect(nh).toBeTruthy();
+    let serviceServer = nh.advertiseService("test_service", "std_srvs/SetBool", (req, res)=>{
+        expect(req.data).toEqual(true)
+        res.success = true;
+        res.message = "Set the bool!";
+        return true;
+    })
+    nh.waitForService("/test_service").then((success)=>{
+        if (success){
+            let serviceClient = nh.serviceClient("/test_service", "std_srvs/SetBool")
+            serviceClient.call({data:true}).then((res)=>{
+                expect(res).toBeTruthy()
+                expect(res.success).toEqual(true)
+                expect(res.message).toEqual("Set the bool!")
+                done()
+            })
+        }
+    })
+})
+afterAll(async ()=>{
+    await rosnodejs.shutdown();
+    await server.stop();
+    return 0;
+})
