4220249d098dc84b9f5fe2105e5858c01901b80f
Nick Zatkovich <NZatkovich@gmail.com>
2019-03-06 15:02:20 -0800

    test_rosmaster

--- tests/__tests__/test_rosmaster-rosmaster-test.js
+++ tests/__tests__/test_rosmaster-rosmaster-test.js
@@ -2,6 +2,7 @@ const debug = require('debug')('test:test_rosmaster:rosmaster')
 const Server = require('../../src/server')
 const rosnodejs = require('rosnodejs')
 const exec = require('await-exec');
+const parseRosTestOutput = require('../rosTestParser.js').parseRosTestOutput
 const { spawn } = require('child_process')
 
 let nh;
@@ -15,41 +16,18 @@ execEnv.ROSCONSOLE_STDOUT_LINE_BUFFERED = 1
 beforeAll(async ()=>{
    server = new Server(config);
    await server.start();
-   nh = await rosnodejs.initNode('/test_rosnodejs_roscpp_client', {rosMasterUri: ROS_MASTER_URI});
+   nh = await rosnodejs.initNode('/test_rosmaster', {rosMasterUri: ROS_MASTER_URI});
 })
 
-it('vector test', async ()=>{
+it('test_rosmaster rosmaster.test', async ()=>{
     let setParamReply = await nh.setParam('/test/joints', ['one', 'two'])
     expect(setParamReply).toEqual([1, 'param set at \'/test/joints\'', 0])
-    const output = await exec('rosrun param_tester param_tester_vector_node', 
+    const output = await exec('rostest -r test_rosmaster rosmaster.test', 
                         {env: execEnv})
-    expect(output.stdout).toEqual('one two ')
-})
-
-
-it('param subscription', done =>{
-    let subprocess = spawn('rosrun', ['param_tester', 'param_tester_paramSub_node'], {env: execEnv})
-    subprocess.stdout.on('data', (data)=>{
-        if (data.includes("Subscribed to parameter")){
-            nh.setParam('/test_node/test_param', "hello").then((reply)=>{
-                expect(reply).toEqual([1, 'param set at \'/test_node/test_param\'', 0])
-            });        
-        } else if (!data.includes("Created new parameter through XmlRpc API call.")){
-            fail(new Error("unexpected output"))
-        }
-    })
-    subprocess.stderr.on("data", (data)=>{
-        if (data.includes("Updated parameter: hello")){
-            subprocess.kill('SIGTERM')
-            done()
-        } else {
-            subprocess.kill('SIGTERM')
-            fail(new Error("unexpected output"))
-        }
-    })
-    subprocess.on('error', (err)=>{
-        fail(new Error("process failed"))
-    })
+    const testOutput = parseRosTestOutput(output.stdout);
+    if (testOutput.summary.result != "success"){
+        
+    }
 })
 
 afterAll(async ()=>{
--- tests/rosTestParser.js
+++ tests/rosTestParser.js
@@ -1,30 +1,30 @@
 
 exports.parseRosTestOutput = function (input){
-    let startLineRe = /\[ROSTEST\]\-\-\-/
-    let testLineRe = /\[(.*)\]\[(.*)\]/
-    let inputLines = input.split('\n')
+    const startLineRe = /\[ROSTEST\]\-\-\-/
+    const testLineRe = /\[(.*)\]\[(.*)\]/
+    const inputLines = input.split('\n')
     let started = false;
-    let endLineRe = /^\-*$/
+    const endLineRe = /^\-*$/
     let testResults = []
     let summary = {};
     for (let x = 0; x < inputLines.length; x++){
         if (started){
             let matches = inputLines[x].match(testLineRe)
             if (matches){
-                let resultIndex = testResults.push({name: match[1], result: match[2], error: ""}) -1;
-                if (matches[2] == FAILURE){
+                let resultIndex = testResults.push({name: matches[1], result: matches[2], error: ""}) -1;
+                if (matches[2] == 'FAILURE'){
                     x++;
                     while (!inputLines[x].match(endLineRe)){
-                        testResults[resultIndex].error += inputLines[x];
+                        testResults[resultIndex].error += inputLines[x] + "\n";
                         x++;
                     }
                 }    
             } else if (inputLines[x].match("SUMMARY")){
                 summary = {
-                    result: inputLines[++x].match(/\* RESULT: (.*)\n/)[1],
-                    tests: inputLines[++x].match(/\* TESTS: (.*)\n/)[1],
-                    errors: inputLines[++x].match(/\* ERRORS: (.*)\n/)[1],
-                    failures: inputLines[++x].match(/\* FAILURES: (.*)\n/)[1],
+                    result: inputLines[++x].match(/ \* RESULT\: (.*)/)[1],
+                    tests: inputLines[++x].match(/ \* TESTS\: (.*)/)[1],
+                    errors: inputLines[++x].match(/ \* ERRORS\: (.*)/)[1],
+                    failures: inputLines[++x].match(/ \* FAILURES\: (.*)/)[1],
                 }
             }
         }
