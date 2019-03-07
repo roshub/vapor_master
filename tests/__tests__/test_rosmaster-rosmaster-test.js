41f617ecc293a434b213c927041265ae41dba117
Nick Zatkovich <NZatkovich@gmail.com>
2019-03-06 18:03:30 -0800

    test_rosmaster

--- tests/__tests__/test_rosmaster-rosmaster-test.js
+++ tests/__tests__/test_rosmaster-rosmaster-test.js
@@ -12,6 +12,9 @@ let ROS_MASTER_URI = __TEST_MASTER_CONFIG__.ROS_MASTER_URI
 let execEnv = process.env
 execEnv.ROS_MASTER_URI = ROS_MASTER_URI
 execEnv.ROSCONSOLE_STDOUT_LINE_BUFFERED = 1
+let saveConsoleError = console.error
+console.error = jest.fn();
+
 
 beforeAll(async ()=>{
    server = new Server(config);
@@ -22,13 +25,39 @@ beforeAll(async ()=>{
 it('test_rosmaster rosmaster.test', async ()=>{
     let setParamReply = await nh.setParam('/test/joints', ['one', 'two'])
     expect(setParamReply).toEqual([1, 'param set at \'/test/joints\'', 0])
-    const output = await exec('rostest -r test_rosmaster rosmaster.test', 
+    let output = null;
+    try {
+         output = await exec('rostest -r test_rosmaster rosmaster_registerPublisherinvalid.test',
                         {env: execEnv})
-    const testOutput = parseRosTestOutput(output.stdout);
-    if (testOutput.summary.result != "success"){
-        
+    } catch (e) {
+        //ignore the error code
+        output = { stdout: e.stdout }
     }
-})
+
+    const testOutput = parseRosTestOutput(output.stdout);
+    let resultsString = ""
+
+    for (testResult of testOutput.results){
+        resultsString = resultsString + testResult.name + ": " + testResult.result + "\n"
+        if (testResult.result != 'passed'){
+            resultsString += "\n" + testResult.error + "\n"
+        }
+    }
+
+    resultsString += "\nSUMMARY\n"
+    resultsString += "result: " + testOutput.summary.result + "\n"
+    resultsString += "tests: " + testOutput.summary.tests + "\n"
+    resultsString += "errors: " + testOutput.summary.errors + "\n"
+    resultsString += "failures: " + testOutput.summary.failures + "\n"
+
+    if (testOutput.summary.result == "passed"){
+        console.log(resultsString)
+    } else {
+        console.error = saveConsoleError
+        console.error(testOutput)
+        fail("rosmaster.test failed: \n" + resultsString);
+    }
+},30000)
 
 afterAll(async ()=>{
     await rosnodejs.shutdown();
